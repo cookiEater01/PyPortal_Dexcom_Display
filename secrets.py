27890361fb44578cea24bccda694d6b7f4f99743
David <dk9946@student.uni-lj.si>
2022-11-13 10:43:40 +0100

    empty secrets file

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    "ssid": "<<WiFi name>>",
+    "password": "<<WiFi password>>",
+    "dexcom_username": "<<Dexcom username>>",
+    "dexcom_password": "<<Dexcom password>>",
+}
