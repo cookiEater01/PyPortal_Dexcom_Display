44b2d15856a8ac83ff60a22361f113ab043d60ab
David <dk9946@student.uni-lj.si>
2022-11-13 19:28:36 +0100

    init

---
+++ const.py
@@ -0,0 +1,10 @@
+# Constants
+
+DEXCOM_URL = "https://shareous1.dexcom.com/ShareWebServices/Services/"
+DEXCOM_URL_US = "https://share2.dexcom.com/ShareWebServices/Services/"
+
+DEXCOM_APPLICATION_ID = "d8665ade-9673-4e27-9ff6-92db4ce13d13"
+
+DEXCOM_LOGIN_ID_ENDPOINT = "General/LoginPublisherAccountById"
+DEXCOM_AUTHENTICATE_ENDPOINT = "General/AuthenticatePublisherAccount"
+DEXCOM_GLUCOSE_READINGS_ENDPOINT = "Publisher/ReadPublisherLatestGlucoseValues"
