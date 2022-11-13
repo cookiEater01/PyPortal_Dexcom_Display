9df9540fc3c757a8df0bf85912c94c5ca2bb82fd
David <dk9946@student.uni-lj.si>
2022-11-13 19:28:23 +0100

    add utils for time and conversions

---
+++ utils.py
@@ -0,0 +1,14 @@
+from adafruit_datetime import datetime
+
+# http://www.bcchildrens.ca/endocrinology-diabetes-site/documents/glucoseunits.pdf
+def mgdl_to_mmol(mgdl: int):
+    return round(mgdl / 18, 1)
+
+
+def get_dt_from_epoch(rsp: str):
+    correct_rsp = rsp.strip("\)").replace("Date(", "")
+
+    dt = datetime.fromtimestamp(int(correct_rsp) / 1000.0)
+    dt_iso = dt.isoformat()
+
+    return dt_iso
