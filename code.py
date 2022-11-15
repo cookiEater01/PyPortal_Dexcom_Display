4a800ca151fd9997fdf79cfff999858c02303b4e
David <dk9946@student.uni-lj.si>
2022-11-15 19:54:58 +0100

    Update code.py

--- code.py
+++ code.py
@@ -12,11 +12,12 @@ import adafruit_requests as requests
 import adafruit_touchscreen
 import displayio
 from adafruit_pyportal import PyPortal
-from display_utils import black_background, text_box, set_image
+from display_utils import black_background, text_box, load_sprite_sheet
 from adafruit_display_text.label import Label
 from adafruit_bitmap_font import bitmap_font
 from adafruit_button import Button
-from adafruit_display_shapes.circle import Circle
+from sprites import Sprites
+import terminalio
 
 # Dexcom
 from dexcom import Dexcom, GlucoseValue
@@ -25,8 +26,10 @@ from dexcom import Dexcom, GlucoseValue
 import rtc
 from adafruit_datetime import datetime, timedelta
 
-#Sensors
+# Sensors
 from analogio import AnalogIn
+
+from random import randrange
 
 
 print("Starting up...")
@@ -65,44 +68,46 @@ ts = adafruit_touchscreen.Touchscreen(
 pyportal = PyPortal(esp=esp, external_spi=spi)
 
 # Set the font and preload letters
-font = bitmap_font.load_font("/fonts/VCROSDMono-17a.bdf")
-font.load_glyphs(b'abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890- ()')
-
 font_glucose = bitmap_font.load_font("/fonts/OpenSans-Regular-50.bdf")
-font_glucose.load_glyphs(b'lmoLM1234567890- ().')
+font_glucose.load_glyphs(b"lmoLM1234567890- ().")
 
-# Root DisplayIO
-root_group = displayio.Group()
-display.show(root_group)
+# Main (Screen) DisplayIO Group
+screen_group = displayio.Group()
+display.show(screen_group)
 
-# Create a new DisplayIO group
-splash = displayio.Group()
-splash.append(black_background(displayio, WIDTH, HEIGHT))
+# Add background to main screen
+screen_group.append(black_background(displayio, WIDTH, HEIGHT))
 
+# Define two views
 loading_view = displayio.Group()
 glucose_view = displayio.Group()
 
-splash.append(loading_view)
+screen_group.append(loading_view)
 
-image_group = displayio.Group()
-image_group.x = 110
-image_group.y = 40
-loading_view.append(image_group)
-set_image(image_group, "/images/wifi.bmp", displayio)
+# Group for loading image(s)
+loading_image_group = displayio.Group()
+loading_image_group.x = 110
+loading_image_group.y = 40
 
-label_group = displayio.Group()
+# Loading images sprites
+loading_tg = load_sprite_sheet("/images/loading_sprites.bmp", 100, 100)
+loading_sprites = Sprites(loading_tg, 3, loading_image_group)
+loading_sprites.add_to_group()
 
-status_label = Label(font)
-status_label.text = "Connecting to WIFI ..."
-status_label.anchor_point = (0.5, 0.5)
-status_label.anchored_position = (WIDTH / 2, HEIGHT * 3 / 4)
-label_group.append(status_label)
+# Add loading image group to  loading view
+loading_view.append(loading_image_group)
 
-# append label_group to splash
-loading_view.append(label_group)
+# Group for loading label
+loading_label_group = displayio.Group()
 
-# Show the group
-display.show(splash)
+loading_status_label = Label(terminalio.FONT)
+loading_status_label.text = "Connecting to WIFI ..."
+loading_status_label.anchor_point = (0.5, 0.5)
+loading_status_label.anchored_position = (WIDTH / 2, HEIGHT * 3 / 4)
+loading_label_group.append(loading_status_label)
+
+# Append loading_label_group to loading view
+loading_view.append(loading_label_group)
 
 # Backlight function
 # Value between 0 and 1 where 0 is OFF, 0.5 is 50% and 1 is 100% brightness.
@@ -132,9 +137,9 @@ while not esp.is_connected:
 print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
 print("My IP address is", esp.pretty_ip(esp.ip_address))
 
-set_image(image_group, "/images/clock.bmp", displayio)
-
-status_label.text = "Acquiring time ..."
+# Show second sprite and update loading text
+loading_sprites.update_tile(1)
+loading_status_label.text = "Acquiring time ..."
 
 # Initialize a requests object with a socket and esp32spi interface
 socket.set_interface(esp)
@@ -142,7 +147,7 @@ requests.set_socket(socket, esp)
 
 # get_time will raise OSError if the time isn't available yet so loop until it works.
 now_utc = None
-print("Waiting for ESP to aquire localtime.", end = " ")
+print("Waiting for ESP to aquire localtime.", end=" ")
 while now_utc is None:
     try:
         esp_time = esp.get_time()
@@ -152,13 +157,21 @@ while now_utc is None:
         pass
 rtc.RTC().datetime = now_utc
 
-set_image(image_group, "/images/dexcom.bmp", displayio)
-
-text_box(status_label, HEIGHT * 3 / 4, 20, "Initializing Dexcom connection ...", pyportal, font)
+# Show last sprite in loading sequence and update and break text
+loading_sprites.update_tile(2)
+text_box(
+    loading_status_label,
+    HEIGHT * 3 / 4,
+    20,
+    "Initializing Dexcom connection ...",
+    pyportal,
+    terminalio.FONT,
+)
 
 # Initialize Dexcom connection and retrieves glucose value
-# dexcom_object = Dexcom(secrets["dexcom_username"], secrets["dexcom_password"], requests)
-# glucose_value = dexcom_object.get_latest_glucose_value(requests)
+dexcom_object = Dexcom(secrets["dexcom_username"], secrets["dexcom_password"], requests)
+glucose_value = dexcom_object.get_latest_glucose_value(requests)
+
 
 timer = time.monotonic()
 last_touch = time.monotonic()
@@ -166,25 +179,74 @@ last_touch = time.monotonic()
 print("Starting the program.")
 print("-" * 40)
 
-#Prepare glucose view display
+# Prepare glucose view
 
-circle_main = Circle(160, 120, 90, fill=0xc6c6c6, outline=0xffffff, stroke=2)
-circle_out = Circle(160, 120, 100, fill=None, outline=0xa9a9a9, stroke=12)
-glucose_view.append(circle_main)
-glucose_view.append(circle_out)
+# Glucose image(s) group
+glucose_image_group = displayio.Group()
+glucose_image_group.x = 55 + 17
+glucose_image_group.y = 10
 
-glucose_group = displayio.Group()
+# Add glucose image group to view
+glucose_view.append(glucose_image_group)
+
+# Glucose images sprites
+glucose_tg = load_sprite_sheet("/images/glucose_sprites.bmp", 200, 222)
+glucose_sprites = Sprites(glucose_tg, 24, glucose_image_group)
+glucose_sprites.add_to_group()
+
+# Group for glucose value
+glucose_value_group = displayio.Group()
 
 glucose_label = Label(font_glucose)
-glucose_label.text = "8.4"
+glucose_label.text = ""
+glucose_label.color = 0x000000
 glucose_label.anchor_point = (0.5, 0.5)
-glucose_label.anchored_position = (180, 120)
-# glucose_group.append(glucose_label)
+glucose_label.anchored_position = (156, 121)
 
-glucose_view.append(glucose_label)
+# Add glucose value label to group
+glucose_value_group.append(glucose_label)
 
-splash.remove(loading_view)
-splash.append(glucose_view)
+# Add glucose value group to view
+glucose_view.append(glucose_value_group)
+
+# Group for glucose unit
+glucose_unit_group = displayio.Group()
+
+glucose_unit_label = Label(terminalio.FONT)
+glucose_unit_label.text = "mmol/L"
+glucose_unit_label.color = 0x000000
+glucose_unit_label.anchor_point = (0.5, 0.5)
+glucose_unit_label.anchored_position = (156, 165)
+
+# Add glucose unit label to group
+glucose_unit_group.append(glucose_unit_label)
+
+# Add glucose unit group to view
+glucose_view.append(glucose_unit_group)
+
+# Group for last glucose update
+glucose_update_group = displayio.Group()
+
+glucose_update_label = Label(terminalio.FONT)
+glucose_update_label.text = ""
+glucose_update_label.color = 0xFFFFFF
+glucose_update_label.anchor_point = (0.5, 0.5)
+glucose_update_label.anchored_position = (156, 230)
+
+# Add glucose unit label to group
+glucose_update_group.append(glucose_update_label)
+
+# Add glucose unit group to view
+glucose_view.append(glucose_update_group)
+
+# Show correct values on screen
+glucose_value.update_view(
+    glucose_sprites, glucose_label, glucose_unit_label, glucose_update_label
+)
+
+# Switch views
+screen_group.remove(loading_view)
+screen_group.append(glucose_view)
 
 while True:
     p = ts.touch_point
@@ -196,8 +258,12 @@ while True:
             set_backlight(0.0)
         else:
             set_backlight(0.9)
-    if time.monotonic() - timer >= 10.0:
+    # Prevent to frequent update calls, wait for 10s between API calls
+    # Disable update when screen is off
+    if time.monotonic() - timer >= 30.0 and board.DISPLAY.brightness > 0.0:
         timer = time.monotonic()
-        # if datetime.now() >= glucose_value.next_fetch:
-        #    glucose_value = dexcom_object.get_latest_glucose_value(requests)
-
+        if datetime.now() >= glucose_value.next_fetch:
+            glucose_value = dexcom_object.get_latest_glucose_value(requests)
+            glucose_value.update_view(
+                glucose_sprites, glucose_label, glucose_unit_label, glucose_update_label
+            )
