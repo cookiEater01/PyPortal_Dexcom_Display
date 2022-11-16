75c6307cb03c76407497afecfcf36d4017835640
David <koprivecd@gmail.com>
2022-11-16 21:50:37 +0100

    new const for timezone

--- const.py
+++ const.py
@@ -8,3 +8,6 @@ DEXCOM_APPLICATION_ID = "d8665ade-9673-4e27-9ff6-92db4ce13d13"
 DEXCOM_LOGIN_ID_ENDPOINT = "General/LoginPublisherAccountById"
 DEXCOM_AUTHENTICATE_ENDPOINT = "General/AuthenticatePublisherAccount"
 DEXCOM_GLUCOSE_READINGS_ENDPOINT = "Publisher/ReadPublisherLatestGlucoseValues"
+
+WORLD_TIME_API_ENDPOINT = "http://worldtimeapi.org/api/timezone/"
+TIMEZONE = "Europe/Ljubljana"
