cf37b43166b5b75eecc77bcf1315f24d91cc8858
David <koprivecd@gmail.com>
2023-12-09 19:24:36 +0100

    remove old and unneeded files

--- session.py
+++
@@ -1,9 +0,0 @@
-from adafruit_datetime import datetime, timedelta
-
-class Session:
-    def __init__(self, session_id, last_used_time):
-        self.session_id = session_id
-        self.last_used_time = last_used_time
-
-    def is_session_valid(self):
-        return False if datetime.now() > self.last_used_time + timedelta(hours=1) else True
