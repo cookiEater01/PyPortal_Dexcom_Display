c1235b66563ff0129a874689b03957f807f0346f
David <koprivecd@gmail.com>
2024-06-02 19:22:07 +0200

    using new fonts

--- display_utils.py
+++ display_utils.py
@@ -11,11 +11,22 @@ def play_tap_sound(pyportal):
     pyportal.play_file(soundBeep)
     print("Touch detected.")
 
+def load_symbols_main():
+    # Set the font and preload letters
+    font = bitmap_font.load_font("/fonts/Roboto-Bold-60.bdf")
+    font.load_glyphs(b"1234567890.-M")
+    return font
+
+def load_symbols_decimal():
+    # Set the font and preload letters
+    font = bitmap_font.load_font("/fonts/Roboto-Medium-40.bdf")
+    font.load_glyphs(b"1234567890M")
+    return font
 
 def load_symbols():
     # Set the font and preload letters
-    font = bitmap_font.load_font("/fonts/OpenSans-Regular-50.bdf")
-    font.load_glyphs(b"lmoLM1234567890- ().")
+    font = bitmap_font.load_font("/fonts/Roboto-Regular-12.bdf")
+    font.load_glyphs(b"lmoLM1234567890- ()/.")
     return font
 
 
@@ -26,7 +37,7 @@ def create_and_show_group(display, width, height):
     return group
 
 
-def add_black_background(width, height, colour=0x0):
+def add_black_background(width, height, colour=0xf1ecea):
     # Default is black background
     bg_bitmap = displayio.Bitmap(width, height, 1)
     bg_palette = displayio.Palette(1)
@@ -63,40 +74,46 @@ def prepare_label(font, text: str, colour, anchor_point, anchored_position):
     return label
 
 
-def create_glucose_group(font, use_us: bool):
+def create_glucose_group(font_main, font_decimal, font_other, use_us: bool):
     glucose_group = displayio.Group()
 
     # Groups
-    sleep_icon_group = prepare_group(270, 0)
-    glucose_image_group = prepare_group(55 + 17, 10)
+    glucose_image_group = prepare_group(37, 10)
     glucose_value_group = displayio.Group()
     glucose_unit_group = displayio.Group()
+    sleep_icon_group = prepare_group(270, 0)
     glucose_update_dt_group = displayio.Group()
     warning_icon_group = prepare_group(0, 0)
     refreshing_icon_group = prepare_group(0, 190)
+
+
+    # Glucose image sprites
+    glucose_tg = load_sprite_sheet("/images/glucose_sprites.bmp", 246, 220)
+    glucose_sprites = Sprites(glucose_tg, 24, glucose_image_group)
+    glucose_sprites.add_to_group()
+
+    # Glucose - main - value
+    glucose_main_label = prepare_label(font_main, "", 0x000000, (0.0, 0.0), (56, 25))
+    glucose_value_group.append(glucose_main_label)
+
+    # Glucose - decimal - value
+    glucose_decimal_label = prepare_label(font_decimal, "", 0x000000, (1.0, 0.0), (56, 37))
+    glucose_value_group.append(glucose_decimal_label)
+
+    # Glucose unit
+    glucose_unit_label = prepare_label(font_other, "mmol/L" if use_us is False else "mg/dl",
+                                       0x616d7c, (0.5, 0.5), (160, 165))
+    glucose_unit_group.append(glucose_unit_label)
+
+    # Glucose datetime
+    glucose_update_dt_label = prepare_label(font_other, "", 0x000000, (0.5, 0.5), (160, 230))
+    glucose_update_dt_group.append(glucose_update_dt_label)
 
     # Sleep button
     sleep_icon_tg = load_sprite_sheet("/images/sleep.bmp", 50, 50)
     sleep_sprites = Sprites(sleep_icon_tg, 1, sleep_icon_group)
     sleep_sprites.add_to_group()
 
-    # Glucose image sprites
-    glucose_tg = load_sprite_sheet("/images/glucose_sprites.bmp", 200, 222)
-    glucose_sprites = Sprites(glucose_tg, 24, glucose_image_group)
-    glucose_sprites.add_to_group()
-
-    # Glucose value
-    glucose_label = prepare_label(font, "", 0x000000, (0.5, 0.5), (156, 121))
-    glucose_value_group.append(glucose_label)
-
-    # Glucose unit
-    glucose_unit_label = prepare_label(terminalio.FONT, "mmol/L" if use_us is False else "mg/dl",
-                                       0x000000, (0.5, 0.5), (156, 165))
-    glucose_unit_group.append(glucose_unit_label)
-
-    # Glucose datetime
-    glucose_update_dt_label = prepare_label(terminalio.FONT, "", 0xFFFFFF, (0.5, 0.5), (156, 230))
-    glucose_update_dt_group.append(glucose_update_dt_label)
 
     # Warning out of date
     warning_icon_tg = load_sprite_sheet("/images/warning.bmp", 50, 50)
@@ -107,19 +124,19 @@ def create_glucose_group(font, use_us: bool):
     refreshing_sprites = Sprites(refreshing_icon_tg, 1, refreshing_icon_group)
 
     # Add subgroups to main group
-    glucose_group.append(sleep_icon_group)
     glucose_group.append(glucose_image_group)
     glucose_group.append(glucose_value_group)
     glucose_group.append(glucose_unit_group)
     glucose_group.append(glucose_update_dt_group)
+    glucose_group.append(sleep_icon_group)
     glucose_group.append(warning_icon_group)
     glucose_group.append(refreshing_icon_group)
 
-    return (glucose_group, glucose_sprites, glucose_label, glucose_unit_label,
+    return (glucose_group, glucose_sprites, glucose_main_label, glucose_decimal_label, glucose_unit_label,
             glucose_update_dt_label, warning_sprites, refreshing_sprites)
 
 
-def create_loading_group(width, height):
+def create_loading_group(width, height, font):
     loading_group = displayio.Group()
     # Group for loading image(s)
     loading_image_group = prepare_group(110, 40)
@@ -134,7 +151,7 @@ def create_loading_group(width, height):
 
     # Group and loading label
     loading_label_group = displayio.Group()
-    loading_status_label = prepare_label(terminalio.FONT, "Connecting to WIFI ...", 0xFFFFFF, (0.5, 0.5),
+    loading_status_label = prepare_label(font, "Connecting to WIFI ...", 0x616d7c, (0.5, 0.5),
                                          (width / 2, height * 3 / 4))
     loading_label_group.append(loading_status_label)
 
