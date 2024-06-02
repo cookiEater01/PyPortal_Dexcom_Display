bb74294348e4209c5b9bd9276c3f73e38d8b5266
David <koprivecd@gmail.com>
2024-06-02 19:22:17 +0200

    using new fonts and colours

--- display_mode.py
+++ display_mode.py
@@ -4,7 +4,7 @@ from dexcom_glucose import DexcomGlucose
 
 
 class DisplayMode:
-    def __init__(self, display, width, height, font, use_us: bool):
+    def __init__(self, display, width, height, font_main, font_decimal, font_other, use_us: bool):
         self.mode = ""
         self.height = height
         self.width = width
@@ -12,12 +12,14 @@ class DisplayMode:
         self.use_us = use_us
         self.loading_displayed = False
         self.glucose_displayed = False
-        self.font = font
+        self.font_main = font_main
+        self.font_decimal = font_decimal
+        self.font_other = font_other
         self.screen_group = create_and_show_group(display, width, height)
-        self.loading_group, self.loading_sprites, self.loading_status_label = create_loading_group(width, height)
-        (self.glucose_group, self.glucose_sprites, self.glucose_label, self.glucose_unit_label,
+        self.loading_group, self.loading_sprites, self.loading_status_label = create_loading_group(width, height, font_other)
+        (self.glucose_group, self.glucose_sprites, self.glucose_main_label, self.glucose_decimal_label, self.glucose_unit_label,
          self.glucose_update_dt_label, self.warning_sprites, self.refreshing_sprites) = (
-            create_glucose_group(font, use_us)
+            create_glucose_group(font_main, font_decimal, font_other, use_us)
         )
 
     def change(self, new_mode: str):
@@ -51,7 +53,7 @@ class DisplayMode:
 
     def display_loading_dexcom(self, py_portal):
         self.loading_sprites.update_tile(2)
-        text_height = Label(self.font, text="M", color=0x03AD31)
+        text_height = Label(self.font_other, text="M", color=0x616d7c)
         text = py_portal.wrap_nicely("Initializing Dexcom connection ...", 20)
         new_text = ""
         test = ""
@@ -69,27 +71,42 @@ class DisplayMode:
         print("Updating displayed glucose")
         if glucose.mmol is None:
             self.glucose_sprites.update_tile(glucose.trend_numeric)
-            self.glucose_label.color = 0x000000
-            self.glucose_unit_label.color = 0x000000
-            self.glucose_label.text = "??"
+            self.glucose_main_label.color = 0x000000
+            self.glucose_unit_label.color = 0x616d7c
+            self.glucose_main_label.text = "--"
+            self.glucose_decimal_label.text = ""
+            self.glucose_main_label.anchored_position = (56, 95)
         else:
             if glucose.mmol <= 4.0:
                 self.glucose_sprites.update_tile(glucose.trend_numeric + 8)
-                self.glucose_label.color = 0xFFFFFF
+                self.glucose_main_label.color = 0xFFFFFF
                 self.glucose_unit_label.color = 0xFFFFFF
             elif glucose.mmol >= 8.0:
                 self.glucose_sprites.update_tile(glucose.trend_numeric + 16)
-                self.glucose_label.color = 0x000000
-                self.glucose_unit_label.color = 0x000000
+                self.glucose_main_label.color = 0x000000
+                self.glucose_unit_label.color = 0x616d7c
             else:
                 self.glucose_sprites.update_tile(glucose.trend_numeric)
-                self.glucose_label.color = 0x000000
-                self.glucose_unit_label.color = 0x000000
+                self.glucose_main_label.color = 0x000000
+                self.glucose_unit_label.color = 0x616d7c
             if self.use_us is False:
-                self.glucose_label.text = str(glucose.mmol)
+                self.glucose_main_label.text = str(glucose.mmol)[0: -1]
+                self.glucose_decimal_label.text = str(glucose.mmol)[-1]
+
+                both_label_width = self.glucose_main_label.bounding_box[2] + self.glucose_decimal_label.bounding_box[2]
+                start_margin = (320 - both_label_width) // 2
+
+                print(both_label_width)
+                print(start_margin)
+
+                self.glucose_main_label.anchored_position = (start_margin, 95)
+                self.glucose_main_label.anchor_point = (0.0, 0.0)
+                self.glucose_decimal_label.anchored_position = (320 - start_margin, 107)
+                self.glucose_decimal_label.anchor_point = (1.0, 0.0)
+
                 self.glucose_unit_label.text = "mmol/L"
             else:
-                self.glucose_label.text = str(glucose.mgdl)
+                self.glucose_main_label.text = str(glucose.mgdl)
                 self.glucose_unit_label.text = "mg/dL"
         tmp_time = glucose.local_time
         self.glucose_update_dt_label.text = "{:02d}:{:02d}".format(
