e859f5621a4c9a072e50f1dcc66ad1a869d5581a
David <koprivecd@gmail.com>
2024-06-02 19:22:32 +0200

    fix time issue

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ import rtc
 from adafruit_datetime import datetime, timedelta
 from time_utils import get_time, get_timezone_offset
 from device_utils import set_up_esp, set_up_led, set_backlight, connect_to_wifi, init_requests
-from display_utils import load_symbols, play_tap_sound
+from display_utils import play_tap_sound, load_symbols_main, load_symbols_decimal, load_symbols
 from glucose_utils import trigger_glucose_update
 from dexcom_session import DexcomSession
 from dexcom_glucose import DexcomGlucose
@@ -18,9 +18,11 @@ print("Starting up...")
 # Board settings
 display_turned_off = False
 
-font = load_symbols()
+font_main = load_symbols_main()
+font_decimal = load_symbols_decimal()
+font_other = load_symbols()
 
-display_mode = DisplayMode(display, width, height, font, False)
+display_mode = DisplayMode(display, width, height, font_main, font_decimal, font_other, False)
 display_mode.change("LOADING")
 
 # Get Wi-Fi details and more from a secrets.py file
@@ -34,7 +36,7 @@ time.sleep(6)
 
 # get_time will raise OSError if the time isn't available yet so loop until it works.
 get_time(esp32, time, rtc)
-last_time_fetch = time.monotonic()
+last_time_fetch = datetime.now()
 offset = get_timezone_offset(requests, time, timedelta)
 
 # Initialize Dexcom connection and retrieves glucose value
@@ -43,7 +45,7 @@ num_of_retries: int = 0
 dexcom_session = DexcomSession(secrets["dexcom_username"], secrets["dexcom_password"])
 
 dexcom_glucose = DexcomGlucose(None, "NotComputable", datetime.now(), datetime.now() - timedelta(minutes=15))
-dexcom_glucose, num_of_retries, next_check = trigger_glucose_update(dexcom_glucose, dexcom_session, display_mode, num_of_retries)
+dexcom_glucose, num_of_retries, next_check = trigger_glucose_update(dexcom_glucose, dexcom_session, display_mode, num_of_retries, offset)
 display_mode.change("GLUCOSE")
 
 display_mode.use_us = dexcom_session.us
@@ -71,19 +73,21 @@ while True:
             set_backlight(0.75, display)
     # Prevent to frequent update calls
     # Disable update when screen is off
-    if time.monotonic() - next_check >= 0.0 and display_turned_off is False:
+    until_update_glucose = datetime.now() - next_check
+    if until_update_glucose.total_seconds() >= 0.0 and display_turned_off is False:
         print("Starting to refetch glucose from API.")
 
         if dexcom_session.is_session_valid() is True:
             dexcom_glucose, num_of_retries, next_check = trigger_glucose_update(dexcom_glucose, dexcom_session,
-                                                                                display_mode, num_of_retries)
+                                                                                display_mode, num_of_retries, offset)
         else:
             dexcom_session.initialize()
             dexcom_glucose, num_of_retries, next_check = trigger_glucose_update(dexcom_glucose, dexcom_session,
-                                                                                display_mode, num_of_retries)
+                                                                                display_mode, num_of_retries, offset)
 
     # Update internal clock, if screen is not turned off
-    if display_turned_off is False and time.monotonic() - last_time_fetch >= 1800:
+    since_update_time = datetime.now() - last_time_fetch
+    if display_turned_off is False and since_update_time.total_seconds() >= 1800:
         print("Performing clock update.")
         get_time(esp32, time, rtc)
-        last_time_fetch = time.monotonic()
+        last_time_fetch = datetime.now()
