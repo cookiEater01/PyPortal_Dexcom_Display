2d2c450d480c4d7d438949e372109c72d206e757
David <koprivecd@gmail.com>
2024-06-08 11:47:40 +0200

    Update display_mode.py
    update default font, add logic for displaying wifi, update glucose display

--- display_mode.py
+++ display_mode.py
@@ -1,6 +1,7 @@
 from display_utils import create_glucose_group, create_and_show_group, create_loading_group
 from adafruit_display_text.label import Label
 from dexcom_glucose import DexcomGlucose
+import terminalio
 
 
 class DisplayMode:
@@ -16,9 +17,9 @@ class DisplayMode:
         self.font_decimal = font_decimal
         self.font_other = font_other
         self.screen_group = create_and_show_group(display, width, height)
-        self.loading_group, self.loading_sprites, self.loading_status_label = create_loading_group(width, height, font_other)
+        self.loading_group, self.loading_sprites, self.loading_status_label = create_loading_group(width, height)
         (self.glucose_group, self.glucose_sprites, self.glucose_main_label, self.glucose_decimal_label, self.glucose_unit_label,
-         self.glucose_update_dt_label, self.warning_sprites, self.refreshing_sprites) = (
+         self.glucose_update_dt_label, self.warning_sprites) = (
             create_glucose_group(font_main, font_decimal, font_other, use_us)
         )
 
@@ -47,14 +48,17 @@ class DisplayMode:
                 self.screen_group.remove(self.loading_group)
                 self.loading_displayed = False
 
-    def display_loading_time(self):
-        self.loading_sprites.update_tile(1)
-        self.loading_status_label.text = "Acquiring time ..."
 
-    def display_loading_dexcom(self, py_portal):
-        self.loading_sprites.update_tile(2)
-        text_height = Label(self.font_other, text="M", color=0x616d7c)
-        text = py_portal.wrap_nicely("Initializing Dexcom connection ...", 20)
+
+    def display_loading_wifi(self):
+        self.loading_sprites.update_tile(0)
+        self.loading_status_label.text = "Connecting to WIFI ..."
+
+
+    def display_loading_mqtt(self, py_portal):
+        self.loading_sprites.update_tile(1)
+        text_height = Label(terminalio.FONT, text="M", color=0x616d7c)
+        text = py_portal.wrap_nicely("Initializing MQTT connection ...", 20)
         new_text = ""
         test = ""
         for w in text:
@@ -68,6 +72,10 @@ class DisplayMode:
         self.loading_status_label.text = new_text
 
     def update_glucose(self, glucose: DexcomGlucose):
+
+        if not self.mode == "GLUCOSE":
+            self.change("GLUCOSE")
+
         print("Updating displayed glucose")
         if glucose.mmol is None:
             self.glucose_sprites.update_tile(glucose.trend_numeric)
@@ -80,38 +88,33 @@ class DisplayMode:
             if glucose.mmol <= 4.0:
                 self.glucose_sprites.update_tile(glucose.trend_numeric + 8)
                 self.glucose_main_label.color = 0xFFFFFF
+                self.glucose_decimal_label.color = 0xFFFFFF
                 self.glucose_unit_label.color = 0xFFFFFF
             elif glucose.mmol >= 8.0:
                 self.glucose_sprites.update_tile(glucose.trend_numeric + 16)
                 self.glucose_main_label.color = 0x000000
+                self.glucose_decimal_label.color = 0x000000
                 self.glucose_unit_label.color = 0x616d7c
             else:
                 self.glucose_sprites.update_tile(glucose.trend_numeric)
                 self.glucose_main_label.color = 0x000000
+                self.glucose_decimal_label.color = 0x000000
                 self.glucose_unit_label.color = 0x616d7c
             if self.use_us is False:
-                self.glucose_main_label.text = str(glucose.mmol)[0: -1]
+                self.glucose_main_label.text = str(glucose.mmol)[0:-1]
                 self.glucose_decimal_label.text = str(glucose.mmol)[-1]
-
-                both_label_width = self.glucose_main_label.bounding_box[2] + self.glucose_decimal_label.bounding_box[2]
-                start_margin = (320 - both_label_width) // 2
-
-                print(both_label_width)
-                print(start_margin)
-
-                self.glucose_main_label.anchored_position = (start_margin, 95)
-                self.glucose_main_label.anchor_point = (0.0, 0.0)
-                self.glucose_decimal_label.anchored_position = (320 - start_margin, 107)
-                self.glucose_decimal_label.anchor_point = (1.0, 0.0)
-
                 self.glucose_unit_label.text = "mmol/L"
             else:
                 self.glucose_main_label.text = str(glucose.mgdl)
                 self.glucose_unit_label.text = "mg/dL"
-        tmp_time = glucose.local_time
-        self.glucose_update_dt_label.text = "{:02d}:{:02d}".format(
-            tmp_time.time().hour, tmp_time.time().minute
-        )
+        both_label_width = self.glucose_main_label.bounding_box[2] + self.glucose_decimal_label.bounding_box[2]
+        start_margin = (320 - both_label_width) // 2
+
+        self.glucose_main_label.anchored_position = (start_margin, 95)
+        self.glucose_main_label.anchor_point = (0.0, 0.0)
+        self.glucose_decimal_label.anchored_position = (320 - start_margin, 107)
+        self.glucose_decimal_label.anchor_point = (1.0, 0.0)
+        self.glucose_update_dt_label.text = glucose.time
 
     def add_warning(self):
         print("Displaying warning icon.")
@@ -120,11 +123,3 @@ class DisplayMode:
     def remove_warning(self):
         print("Removing warning icon.")
         self.warning_sprites.remove_from_group()
-
-    def add_refreshing(self):
-        print("Displaying refreshing icon.")
-        self.refreshing_sprites.add_to_group()
-
-    def remove_refreshing(self):
-        print("Removing refreshing icon.")
-        self.refreshing_sprites.remove_from_group()
